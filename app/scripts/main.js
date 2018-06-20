275938b25131d8caa1c67cd79b68e1d8d5ebe9c2
leocantthinkofaname <i7851@hotmail.com>
2018-06-20 15:50:10 +0800

    modify js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,361 +3,361 @@ let getMarkers = () => {
         {
             title: 'Taipei Main Station',
             position: {lat: 25.0478895, lng: 121.5163595},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Taipei Main Station'
         },
         {
             title: 'Smith & Hsu Tea House',
             position: {lat: 25.0423383, lng: 121.5122618},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Smith & Hsu Tea House'
         },
         {
             title: 'Hua Shan 1914',
             position: {lat: 25.0440934, lng: 121.5294619},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Hua Shan 1914'
         },
         {
             title: 'Breeze Center',
             position: {lat: 25.0459398, lng: 121.5444454},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Breeze Center'
         },
         {
             title: 'Red 99 Spicy Hot Pot',
             position: {lat: 25.0426811, lng: 121.5540633},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Red 99 Spicy Hot Pot'
         },
         {
             title: 'Tasters',
             position: {lat: 25.1161711, lng: 121.5277789},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Tasters'
         },
         {
             title: 'Korea Flavor',
             position: {lat: 25.1152118, lng: 121.5328082},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Korea Flavor'
         },
         {
             title: 'AVEDA',
             position: {lat: 25.0394317, lng: 121.5460338},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'AVEDA'
         },
         {
             title: 'MOS Burger',
             position: {lat: 25.0537808, lng: 121.5495158},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'MOS Burger'
         },
         {
             title: 'Don Mono',
             position: {lat: 25.0428628, lng: 121.5045699},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Don Mono'
         },
         {
             title: 'kawaue',
             position: {lat: 25.0552283, lng: 121.5499282},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'kawaue'
         },
         {
             title: 'Beef Noodle',
             position: {lat: 25.0574109, lng: 121.5225547},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Beef Noodle'
         },
         {
             title: 'Utada',
             position: {lat: 25.0318452, lng: 121.4331489},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Utada'
         },
         {
             title: 'Song Shan Cultural Park',
             position: {lat: 25.044317, lng: 121.55978187},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Song Shan Cultural Park'
         },
         {
             title: 'Drip Cafe',
             position: {lat: 25.0435472, lng: 121.5626359},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Drip Cafe'
         },
         {
             title: 'Le Ble\'Dor',
             position: {lat: 25.013457, lng: 121.466232},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Le Ble\'Dor'
         },
         {
             title: 'Hoshina',
             position: {lat: 25.0593316, lng: 121.5443843},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Hoshina'
         },
         {
             title: 'Komonoya',
             position: {lat: 25.0517073, lng: 121.5431137},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Komonoya'
         },
         {
             title: 'Hot Pot',
             position: {lat: 25.0616863, lng: 121.5335667},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Hot Pot'
         },
         {
             title: 'Hot Pot',
             position: {lat: 25.0655043, lng: 121.5243196},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Hot Pot'
         },
         {
             title: 'EXPO Park',
             position: {lat: 25.0703414, lng: 121.5247863},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'EXPO Park'
         },
         {
             title: 'Sugamo Sushi',
             position: {lat: 25.0622876, lng: 121.5322322},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Sugamo Sushi'
         },
         {
             title: 'Miha Honke',
             position: {lat: 25.0542031, lng: 121.5236626},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Miha Honke'
         },
         {
             title: 'Weird Massage House',
             position: {lat: 25.0623085, lng: 121.531517},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Weird Massage House'
         },
         {
             title: 'Toriyaki',
             position: {lat: 25.0449305, lng: 121.5503896},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Toriyaki'
         },
         {
             title: 'Haka Manor',
             position: {lat: 24.4711218, lng: 120.8007147},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Haka Manor'
         },
         {
             title: 'Jiufen Old Street',
             position: {lat: 25.109562, lng: 121.8445791},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Jiufen Old Street'
         },
         {
             title: 'Shihfen Old Streer',
             position: {lat: 25.0424651, lng: 121.7758524},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Shihfen Old Streer'
         },
         {
             title: 'Queen Village',
             position: {lat: 24.9177441, lng: 121.4332971},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Queen Village'
         },
         {
             title: 'Yushan',
             position: {lat: 24.9300026, lng: 121.3538259},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Yushan'
         },
         {
             title: 'Batiau',
             position: {lat: 24.9306415, lng: 121.3771776},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Batiau'
         },
         {
             title: 'Lai\'s Sausage',
             position: {lat: 25.0019627, lng: 121.5797826},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Lai\'s Sausage'
         },
         {
             title: 'Casablanca',
             position: {lat: 25.2506882, lng: 121.4667284},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Casablanca'
         },
         {
             title: 'Sheraton',
             position: {lat: 25.044596, lng: 121.5221746},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Sheraton'
         },
         {
             title: 'Weird Tea House',
             position: {lat: 25.0317146, lng: 121.4337529},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Weird Tea House'
         },
         {
             title: 'LOUISA',
             position: {lat: 25.0303114, lng: 121.4343852},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'LOUISA'
         },
         {
             title: 'Double Tai',
             position: {lat: 25.031972, lng: 121.4324047},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Double Tai'
         },
         {
             title: 'Hot Pot',
             position: {lat: 25.0415809, lng: 121.4458424},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Hot Pot'
         },
         {
             title: 'FUJI Xerox',
             position: {lat: 25.0709733, lng: 121.450546},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'FUJI Xerox'
         },
         {
             title: 'Carrefour Wholesale',
             position: {lat: 25.0430819, lng: 121.4675346},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Carrefour Wholesale'
         },
         {
             title: 'Hot Pot',
             position: {lat: 25.0494723, lng: 121.4652242},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Hot Pot'
         },
         {
             title: 'Bravo Beer',
             position: {lat: 25.014902, lng: 121.462583},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Bravo Beer'
         },
         {
             title: 'Eslite',
             position: {lat: 25.0120927, lng: 121.4628377},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Eslite'
         },
         {
             title: 'Book Store',
             position: {lat: 25.0234716, lng: 121.4670875},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Book Store'
         },
         {
             title: 'Toriyaki',
             position: {lat: 25.0288414, lng: 121.4710979},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Toriyaki'
         },
         {
             title: 'Starbucks',
             position: {lat: 25.020339, lng: 121.4663202},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Starbucks'
         },
         {
             title: 'Sushiya',
             position: {lat: 25.0200728, lng: 121.4675485},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Sushiya'
         },
         {
             title: 'Toriyaki',
             position: {lat: 25.0209395, lng: 121.4703122},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Toriyaki'
         },
         {
             title: '9% Restaurant & Bar',
             position: {lat: 25.030767, lng: 121.4726469},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: '9% Restaurant & Bar'
         },
         {
             title: 'Yuly',
             position: {lat: 25.0308656, lng: 121.4722352},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Yuly'
         },
         {
             title: 'ikki',
             position: {lat: 25.0124239, lng: 121.4618143},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'ikki'
         },
         {
             title: 'New CB Party',
             position: {lat: 25.0075238, lng: 121.458871},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'New CB Party'
         },
         {
             title: 'Sushiya',
             position: {lat: 25.0307873, lng: 121.4754759},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Sushiya'
         },
         {
             title: '12 Hot Pot',
             position: {lat: 25.0300689, lng: 121.4324296},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: '12 Hot Pot'
         },
         {
             title: 'Saliya',
             position: {lat: 25.0120711, lng: 121.5147716},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Saliya'
         },
         {
             title: 'Toscanini',
             position: {lat: 24.960018, lng: 121.2267962},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Toscanini'
         },
         {
             title: 'SOGO',
             position: {lat: 24.9624994, lng: 121.2233305},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'SOGO'
         },
         {
             title: 'LOUISA',
             position: {lat: 24.9621506, lng: 121.2249429},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'LOUISA'
         },
         {
             title: 'Weird Shoe Store',
             position: {lat: 25.0427487, lng: 121.5121704},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Weird Shoe Store'
         },
         {
             title: 'Gi Guo',
             position: {lat: 25.0423739, lng: 121.5137745},
-            icon: './images/smiley.png',
+            icon: '../images/smiley.png',
             content: 'Gi Guo'
         }
     ]
